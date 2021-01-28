68cbdac069c25ec9406ade06c7f89e2bb3753831
Ulises Galeano <34582172+tamalerhino@users.noreply.github.com>
2021-01-28 12:35:15 -0600

    Update and rename briansfreedomcountdown.go to freedomcountdown.go
    Renamed main.go file
    Changed default name to "Ulises"

--- freedomcountdown.go
+++ freedomcountdown.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	deadline := flag.String("deadline", "2020-05-01T00:00:00+01:00", "The deadline for the countdown timer in RFC3339 format (e.g. 2019-12-25T15:00:00+01:00)")
-	name := flag.String("name", "Brian", "The person getting free")
+	deadline := flag.String("deadline", "2021-03-15T00:00:00-05:00", "The deadline for the countdown timer in RFC3339 format (e.g. 2019-12-25T15:00:00+01:00)")
+	name := flag.String("name", "Ulises", "The person getting free")
 	flag.Parse()
 
 	v, err := time.Parse(time.RFC3339, *deadline)
